987bc366903c8696d0b27f19af867ff6fb1f0005
juanurzua94 <39689276+juanurzua94@users.noreply.github.com>
2020-08-25 14:12:16 -0700

    Create RecruitingExercise.java

---
+++ inventory-allocator/src/RecruitingExercise.java
@@ -0,0 +1,21 @@
+public class RecruitingExercise {
+	
+	public static void main(String args[]) {
+		InventoryAllocatorTest.Test1();
+		System.out.println();
+		InventoryAllocatorTest.Test2();
+		System.out.println();
+		InventoryAllocatorTest.Test3();
+		System.out.println();
+		InventoryAllocatorTest.Test4();
+		System.out.println();
+		InventoryAllocatorTest.Test5();
+		System.out.println();
+		InventoryAllocatorTest.Test6();
+		System.out.println();
+		InventoryAllocatorTest.Test7();
+		System.out.println();
+		InventoryAllocatorTest.Test8();
+	}
+	
+}
